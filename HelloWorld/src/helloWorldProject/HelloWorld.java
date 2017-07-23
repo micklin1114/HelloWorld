2fd46a42b1638be0b9682f7844bfdfb356ff08ee
HsiawYeu <HsiawYeu@192.168.0.4>
2017-07-23 10:56:03 +0800

    third commit

--- HelloWorld/src/helloWorldProject/HelloWorld.java
+++ HelloWorld/src/helloWorldProject/HelloWorld.java
@@ -3,6 +3,7 @@ package helloWorldProject;
 public class HelloWorld {
 
 	/* Add a short comment*/
+	// Add another comment
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
